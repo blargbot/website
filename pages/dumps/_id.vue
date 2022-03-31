df2d16cef1a1352e78ca11e347b0077018a46756
danny-may <dannyjmay97@gmail.com>
2022-03-31 21:46:10 +0100

    Add barebones dump page

--- pages/dumps/_id.vue
+++ pages/dumps/_id.vue
@@ -1,7 +1,43 @@
 <template>
   <div>
     <section>
-      <!-- TODO Dumps/Outputs page -->This will eventually show the output data with id {{ params.id }}
+      <p>Id: {{ id }}</p>
+      <p>Content: {{ content }}</p>
+      <p>Embeds: {{ embeds || [] }}</p>
+      <p>ChannelId: {{ channelid }}</p>
+      <p>Expiry: {{ expiry }}</p>
+      <p>Delta: {{ delta }}</p>
     </section>
   </div>
 </template>
+
+<script>
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import advancedFormat from 'dayjs/plugin/advancedFormat'
+
+dayjs.extend(duration)
+dayjs.extend(relativeTime)
+dayjs.extend(advancedFormat)
+
+export default {
+  async asyncData ({ params, $axios }) {
+    const dump = await $axios.$get('/dumps/' + params.id)
+    let expiry = 'never'
+    let delta = 'never'
+    if (dump.expiry > 0) {
+      expiry = dayjs()
+        .add(dump.expiry, 's')
+        .format('MMMM Do, YYYY')
+      delta = dayjs.duration(dump.expiry, 's').humanize(true)
+    }
+
+    return {
+      ...dump,
+      expiry,
+      delta
+    }
+  }
+}
+</script>
