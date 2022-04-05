d2d5489fe694f0265a0eabfae72b7176e230285d
danny-may <dannyjmay97@gmail.com>
2022-04-05 14:25:30 +0100

    Add ccommand editor

---
+++ components/editor/CrudEditor.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <div class="control-row v-aligned">
+      <label for="tagName" class="col-1">
+        {{ type }} name
+      </label>
+      <input v-model="tagName" name="tagName" class="input col-1">
+      <button class="button col-1" :disabled="!tagName" @click.prevent="load">
+        Load
+      </button>
+      <button class="button col-1" :disabled="!tagName" @click.prevent="save">
+        Save
+      </button>
+    </div>
+    <div class="control-row v-aligned">
+      <label for="newName" class="col-1">
+        New name
+      </label>
+      <input v-model="newName" name="newName" class="input col-1">
+      <button class="button col-1" :disabled="!newName || !tagName" @click.prevent="rename">
+        Rename
+      </button>
+      <button class="button danger col-1 v-align" :disabled="!tagName" @click.prevent="remove">
+        Delete
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: null
+    },
+    route: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      tagName: '',
+      newName: '',
+      loadedTag: null
+    }
+  },
+  computed: {
+    endpoint() {
+      return `/${this.route}/${encodeURIComponent(this.tagName)}`
+    }
+  },
+  mounted() {
+    this.loadedTag = null
+  },
+  methods: {
+    async rename() {
+      if (!this.ensureLoaded('rename')) {
+        return
+      }
+
+      await this.updateContent(async () => {
+        await this.$axios.$patch(this.endpoint, {
+          name: this.newName
+        })
+        this.tagName = this.newName
+        this.newName = null
+      })
+    },
+    async load() {
+      await this.updateContent(async () => {
+        const tag = await this.$axios.$get(this.endpoint)
+        return tag.content
+      })
+    },
+    async remove() {
+      if (!this.ensureLoaded('delete')) {
+        return
+      }
+      await this.updateContent(async () => {
+        await this.$axios.$delete(this.endpoint)
+        return ''
+      }, '')
+    },
+    async save() {
+      if (!this.ensureLoaded('save')) {
+        return
+      }
+      await this.updateContent(async () => {
+        await this.$axios.$put(this.endpoint, {
+          content: this.value
+        })
+      })
+    },
+    ensureLoaded(action) {
+      return (
+        this.loadedTag === this.tagName ||
+        confirm(
+          `You are trying to ${action} the ${this.type} ${this.tagName} before you loaded it, are you sure?`
+        )
+      )
+    },
+    async updateContent(action, fallback) {
+      const loadedTag = this.tagName
+      try {
+        const content = await action()
+        this.loadedTag = loadedTag
+        this.$emit('input', content ?? this.value)
+      } catch (err) {
+        if (!(err instanceof Error)) {
+          throw err
+        }
+        switch (err.message) {
+          case 'Request failed with status code 404':
+            this.loadedTag = loadedTag
+            this.$emit('input', fallback ?? this.value)
+            break
+          case 'Request failed with status code 401':
+            this.$router.app.refresh()
+            break
+          case 'Request failed with status code 403':
+            alert(`You dont own that ${this.type}!`)
+            break
+          default:
+            throw err
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.control-row {
+  display: grid;
+  grid-template-columns: 10% 60% 15% 15%;
+
+  .col-1 {
+    grid-column-end: span 1;
+  }
+  .col-2 {
+    grid-column-end: span 2;
+  }
+}
+</style>
--- components/editor/CustomCommandEditor.vue
+++ components/editor/CustomCommandEditor.vue
@@ -1,30 +1,12 @@
 <template>
-  <div>
-    <div class="control-row v-aligned">
-      <label for="ccName" class="col-1">Command Name</label>
-      <input v-model="ccName" name="ccName" class="input col-1">
-      <button class="button col-1" @click.prevent="loadCC">
-        Load
-      </button>
-      <button class="button col-1" @click.prevent="saveCC">
-        Save
-      </button>
-    </div>
-    <div class="control-row v-aligned">
-      <label for="newName" class="col-1">New name</label>
-      <input v-model="newName" name="newName" class="input col-1">
-      <button class="button col-1" @click.prevent="renameCC">
-        Rename
-      </button>
-      <button class="button danger col-1 v-align" @click.prevent="deleteCC">
-        Delete
-      </button>
-    </div>
-  </div>
+  <crud-editor v-model="valueModel" :route="`guilds/${guildId}/ccommands`" type="Command" />
 </template>
 
 <script>
+import CrudEditor from './CrudEditor.vue'
+
 export default {
+  components: { CrudEditor },
   props: {
     value: {
       type: String,
@@ -32,61 +14,18 @@ export default {
     },
     guildId: {
       type: String,
-      required: true
+      default: null
     }
   },
-  data() {
-    return {
-      ccName: '',
-      newName: '',
-      hasLoaded: false
-    }
-  },
-  mounted() {
-    this.hasLoaded = false
-  },
-  methods: {
-    renameCC() {
-      alert(
-        `You are renaming the custom command ${this.commandName} to ${this.newName}`
-      )
-    },
-    loadCC() {
-      this.hasLoaded = true
-      alert(`You are trying to load the custom command ${this.commandName}`)
-    },
-    deleteCC() {
-      if (!this.hasLoaded) {
-        alert(
-          `You are trying to delete the custom command ${this.commandName} before you loaded it`
-        )
-      } else {
-        alert(`You are trying to delete the custom command ${this.commandName}`)
-      }
-    },
-    saveCC() {
-      if (!this.hasLoaded) {
-        alert(
-          `You are trying to save the custom command ${this.commandName} before you loaded it`
-        )
-      } else {
-        alert(`You are trying to save the custom command ${this.commandName}`)
+  computed: {
+    valueModel: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('input', value)
       }
     }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.control-row {
-  display: grid;
-  grid-template-columns: 10% 60% 15% 15%;
-
-  .col-1 {
-    grid-column-end: span 1;
-  }
-  .col-2 {
-    grid-column-end: span 2;
-  }
-}
-</style>
